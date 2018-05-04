8ddff4c3742586dda5e2882bc43f6b56ae7b5983
gasper <bricktowntw@gmail.com>
2018-05-04 16:06:32 +0800

    add Admin::BaseController and move authenticate_admin here

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,6 +1,6 @@
-class Admin::RestaurantsController < ApplicationController
-  
-  before_action :authenticate_admin
+class Admin::RestaurantsController < Admin::BaseController
+
+
   before_action :set_restaurant, only: [:show, :edit, :update, :destory]
 
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = "Not allow!"
-      redirect_to root_path
-    end
-  end
-
+  
   
 end
