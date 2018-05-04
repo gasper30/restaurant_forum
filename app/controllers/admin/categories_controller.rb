756f6eddcb3526cb3a99615bbde8e72d569d28ab
gasper <bricktowntw@gmail.com>
2018-05-04 16:00:24 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,9 @@
 class Admin::CategoriesController < ApplicationController
-  before_action :authenticate_user!
+  
   before_action :authenticate_admin
 
   before_action :set_category, only: [:update, :destroy]
-  
+
   def index
     @categories = Category.all
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :authenticate_user!
+
   def authenticate_admin
     unless current_user.admin?
       flash[:alert] = "Not allow!"
