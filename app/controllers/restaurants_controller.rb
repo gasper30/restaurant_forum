a2b45bb3dba349933dd5cee3a921c42ae2d9ce2a
gasper <bricktowntw@gmail.com>
2018-05-22 15:26:11 +0800

    user can view top10 favorited restaurant on ranking page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -37,6 +37,11 @@ class RestaurantsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  # GET /restaurants/ranking
+  def ranking
+    @restaurants = Restaurant.order(favorites_count: :desc).limit(10)
+  end
+
   def like
     @restaurant = Restaurant.find(params[:id])
     @restaurant.likes.create!(user: current_user)
