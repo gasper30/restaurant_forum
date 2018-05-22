7eb544c7d640d4d586642983c93102436d07408a
gasper <bricktowntw@gmail.com>
2018-05-22 15:39:23 +0800

    update favorites_count using counter_cache

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -14,10 +14,7 @@ class Restaurant < ApplicationRecord
     self.favorited_users.include?(user)
   end
 
-  def count_favorites
-    self.favorites_count = self.favorites.size
-    self.save
-  end
+  
 
   def is_liked?(user)
     self.liked_users.include?(user)
