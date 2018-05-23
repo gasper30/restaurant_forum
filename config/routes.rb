0ff0c0821849fc178895ee94a606a418fb1611cc
gasper <bricktowntw@gmail.com>
2018-05-23 14:41:00 +0800

    rails generate controller followships

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:index, :show, :edit, :update]
-  resources :followships, only: [:create, :destory]
+  resources :followships, only: [:create, :destroy]
 
 
   resources :categories, only: :show
