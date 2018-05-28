cd7147c053faac46a8be59a0540a2a9b8f0a75d3
gasper <bricktowntw@gmail.com>
2018-05-28 14:11:22 +0800

    modify favorited

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :delete_all
   has_many :restaurants, through: :comments
   has_many :favorites, dependent: :destroy
-  has_many :favorite_restaurants, through: :favorites, source: :restaurant
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
   has_many :likes, dependent: :destroy
   has_many :like_restaurants, through: :likes, source: :restaurant
   has_many :followships, dependent: :destroy
